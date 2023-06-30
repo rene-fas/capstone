1f459088af93e495ed9dad1c1036b4ba56c5bdba
René Faßbender <rene-fassb@gmx.de>
2023-06-30 09:08:53 +0200

    refactor

--- components/FieldTripListPage/index.js
+++ components/FieldTripListPage/index.js
@@ -17,7 +17,6 @@ const FieldTripListPage = () => {
     showPopup: false,
     newFieldTripTitle: "",
     newFieldTripDate: "",
-    dateError: false,
   });
 
   useEffect(() => {
@@ -31,14 +30,11 @@ const FieldTripListPage = () => {
   }, []);
 
   const handleAddFieldTrip = () => {
-    if (
-      fieldtripData.newFieldTripTitle.trim() !== "" &&
-      !fieldtripData.dateError
-    ) {
+    if (fieldtripData.newFieldTripTitle.trim() !== "") {
       const newFieldTrip = {
         id: fieldtripData.fieldtrips.length + 1,
         title: fieldtripData.newFieldTripTitle,
-        date: fieldtripData.newFieldTripDate,
+        date: formatDate(fieldtripData.newFieldTripDate),
       };
       const updatedFieldTrips = [...fieldtripData.fieldtrips, newFieldTrip];
       setFieldtripData((prevData) => ({
@@ -50,6 +46,11 @@ const FieldTripListPage = () => {
       }));
       localStorage.setItem("fieldTrips", JSON.stringify(updatedFieldTrips));
     }
+  };
+
+  const formatDate = (date) => {
+    const [year, month, day] = date.split("-");
+    return `${day}-${month}-${year}`;
   };
 
   const handleTitleChange = (e) => {
@@ -64,7 +65,6 @@ const FieldTripListPage = () => {
     setFieldtripData((prevData) => ({
       ...prevData,
       newFieldTripDate: date,
-      dateError: !isValidDate(date),
     }));
   };
 
@@ -73,23 +73,12 @@ const FieldTripListPage = () => {
       ...prevData,
       newFieldTripTitle: "",
       newFieldTripDate: "",
-      dateError: false,
       showPopup: false,
     }));
   };
 
-  const isValidDate = (date) => {
-    const pattern = /^(0?[1-9]|[12][0-9]|3[01])\.(0?[1-9]|1[0-2])\.\d{4}$/;
-    return pattern.test(date);
-  };
-
-  const {
-    fieldtrips,
-    showPopup,
-    newFieldTripTitle,
-    newFieldTripDate,
-    dateError,
-  } = fieldtripData;
+  const { fieldtrips, showPopup, newFieldTripTitle, newFieldTripDate } =
+    fieldtripData;
 
   return (
     <Container>
@@ -134,18 +123,14 @@ const FieldTripListPage = () => {
             type="text"
             value={newFieldTripTitle}
             onChange={handleTitleChange}
-            placeholder="Enter field trip title"
+            placeholder="Exkursionsname"
           />
           <input
-            type="text"
+            type="date"
             value={newFieldTripDate}
             onChange={handleDateChange}
-            placeholder="Enter date (dd.mm.yyyy)"
-            className={dateError ? "error" : ""}
+            placeholder="dd-mm-yyyy"
           />
-          {dateError && (
-            <p className="error-text">Please enter a valid date (dd.mm.yyyy)</p>
-          )}
           <Button onClick={handleCancelAddFieldTrip}>Cancel</Button>
           <Button onClick={handleAddFieldTrip}>Add</Button>
         </Dialog>
